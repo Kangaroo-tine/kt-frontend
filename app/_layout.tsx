07c59e46d58b3fa3182c78e7eb940a96cd66ab0a
KimEunHye <rladmsgp031219@naver.com>
2025-07-01 05:56:51 +0900

    [MOD]바텀탭 구현에 따른 필요없는 코드 삭제 및 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,13 @@
+import { Slot } from 'expo-router';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+//전역 라우팅 제어, 여기서 어떤 화면으로 갈지 분기
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
@@ -23,10 +24,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <Slot /> {/* 여기서 (tabs)/_layout.tsx를 자동으로 렌더링해줌 */}
       <StatusBar style="auto" />
     </ThemeProvider>
   );
