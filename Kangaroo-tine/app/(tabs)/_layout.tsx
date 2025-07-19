fda91df03111fba3a962f8759eb82fca9c179bec
Youngju Lee <oortmealy@gmail.com>
2025-07-19 09:56:36 +0900

    Revert "UI/15 calendar"

--- Kangaroo-tine/app/(tabs)/_layout.tsx
+++
@@ -1,5 +0,0 @@
-import BottomTabs from './BottomTabs';
-
-export default function Layout() {
-  return <BottomTabs />;
-}
--- Kangaroo-tine/prettier.config.js
+++
@@ -1,9 +0,0 @@
-// prettier.config.js
-module.exports = {
-  plugins: ['@trivago/prettier-plugin-sort-imports'],
-  importOrder: ['^react$', '^react-native$', '^@?\\w', '^[./]'],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-  singleQuote: true,
-  trailingComma: 'all',
-};
