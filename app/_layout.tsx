ec165ebbb6242f793edc623390de8acf0a81bca0
Youngju Lee <oortmealy@gmail.com>
2025-09-27 13:02:01 +0900

    [CHORE] : 빨간줄 에러 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from '../hooks/useColorScheme';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 //전역 라우팅 제어, 여기서 어떤 화면으로 갈지 분기
 import {
@@ -11,7 +11,10 @@ import {
 import { useFonts } from 'expo-font';
 import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import * as SplashScreen from 'expo-splash-screen';
 import 'react-native-reanimated';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -20,6 +23,12 @@ export default function RootLayout() {
     'Pretendard-Medium': require('../assets/fonts/Pretendard-Medium.ttf'),
     'Pretendard-Bold': require('../assets/fonts/Pretendard-Bold.ttf'),
   });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
 
   if (!loaded) {
     return null;
