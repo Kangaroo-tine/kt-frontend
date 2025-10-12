b1a0c9394cb6683ef0ead7ba2e7bfd7983065923
eunhyekimyeah <rladmsgp031219@naver.com>
2025-10-12 23:00:56 +0900

    .

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-import messaging from '@react-native-firebase/messaging';
-
-messaging().setBackgroundMessageHandler(async remoteMessage => {
-  console.log('💤 백그라운드 메시지 수신:', remoteMessage);
-});
+import 'expo-router/entry';
